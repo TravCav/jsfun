067c85bb0a2aa45ca5fc09ac5c517f9aac93931e
TravCav <xurrux@gmail.com>
2019-04-09 17:59:54 -0400

    metrics

--- whatjusthappened/wjh.js
+++ whatjusthappened/wjh.js
@@ -9,7 +9,7 @@ let centerX = ctx.canvas.width / 2;
 let centerY = ctx.canvas.height / 2;
 let pixels = ctx.createImageData(ctx.canvas.width, ctx.canvas.height);
 
-let scale = 10000;
+let scale = localStorage.getItem("scale") || 2500000;
 
 let locations = [];
 let storageLocations = JSON.parse(localStorage.getItem("locations"));
@@ -30,12 +30,17 @@ function AddTestData() {
         ay += GetRand();
         nx += ax;
         ny += ay;
-        locations.push({ lat: nx, long: ny, dt: Date.now() });    
-    }   
+        locations.push({
+            lat: nx,
+            long: ny,
+            dt: Date.now()
+        });
+    }
 }
 
-function Scale(amount){
+function Scale(amount) {
     scale *= amount;
+    localStorage.setItem("scale", scale);
     DrawScreen();
 }
 
@@ -70,6 +75,13 @@ function GetLocation() {
 
 function GetScore() {
     total = 0;
+    let furthest = {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+    };
+
     const lastPosition = locations[locations.length - 1];
     for (let index = 1, locLen = locations.length; index < locLen; index++) {
         const curr = locations[index];
@@ -78,26 +90,43 @@ function GetScore() {
         const dy = curr.lat - prev.lat;
         const dv = Math.sqrt((dx * dx) + (dy * dy));
         total += dv;
+
+        if ( furthest.left === 0 || furthest.left > curr.long){
+            furthest.left = curr.long;
+        }
+        if ( furthest.right === 0 || furthest.right < curr.long){
+            furthest.right = curr.long;
+        }
+        
+        if ( furthest.top === 0 || furthest.top > curr.lat){
+            furthest.top = curr.lat;
+        }
+        if ( furthest.bottom === 0 || furthest.bottom < curr.lat){
+            furthest.bottom = curr.lat;
+        }
     }
 
-    document.getElementById("pointsP").innerText = (total * 100).toFixed(2);
+    const longDiff = furthest.left + "  :  " + furthest.right + " " + (furthest.right - furthest.left) * 10000;
+    const latDiff = furthest.top + "  :  " + furthest.bottom + " " + (furthest.bottom - furthest.top) * 10000;
+
+    document.getElementById("pointsP").innerHTML = (total * 100).toFixed(2);// + "<br/>" + longDiff + "<br/>" + latDiff;
 }
 
 function PutPixel(x, y) {
     x = Math.floor(x);
     y = Math.floor(y);
     if (!(
-        x < 1 ||
-        y < 1 ||
-        x > ctx.canvas.width ||
-        y > ctx.canvas.height
-    )) {
+            x < 1 ||
+            y < 1 ||
+            x > ctx.canvas.width ||
+            y > ctx.canvas.height
+        )) {
         const i = (x + y * ctx.canvas.width) * 4;
         pixels.data[i] = 255;
         pixels.data[i + 1] = 255;
         pixels.data[i + 2] = 255;
         pixels.data[i + 3] = 255;
-    ////} else {
+        ////} else {
         ////Scale(0.9);
     }
 }
@@ -107,19 +136,23 @@ function ShowPosition(position) {
     let lastLoc = locations[locations.length - 1];
 
     if (locations.length === 0 || (position.coords.latitude !== lastLoc.lat && position.coords.longitude !== lastLoc.long)) {
-        locations.push({ lat: position.coords.latitude, long: position.coords.longitude, dt: Date.now() });
+        locations.push({
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+            dt: Date.now()
+        });
         let jsonLoc = JSON.stringify(locations);
         localStorage.setItem("locations", jsonLoc);
     }
 
-    ////AddTestData();
+    AddTestData();
 
     DrawScreen();
 
 }
 
-function GetRand(){
-    return ((Math.random() * 2) - 1) * .0000001;
+function GetRand() {
+    return (Math.random() - .5) * .0000001;
 }
 
 DrawScreen();
